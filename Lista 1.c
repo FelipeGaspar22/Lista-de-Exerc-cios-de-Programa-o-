9c3e49cf7bcd631816df8c24c154c93f7576f806
SR_Gaspar <estudos.felipe.gaspar@gmail.com>
2026-08-31 18:55:05 -0300

    Create Lista 1.c

---
+++ Lista 1.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/*
+
+Lista 1 - Exercício 1
+
+int main()
+{
+    int number1, number2;
+    printf("Insira o valor de N1: ");
+    scanf("%d", &number1);
+    printf("Insira o valor de N2: ");
+    scanf("%d", &number2);
+    
+    printf("A inversão da ordem dos numeros inseridos é %d e %d", number2, number1);
+
+    return 0;
+}
+
+*/
