067c8b85908b5c264c90b8a540c9b60a7fb8309c
SR_Gaspar <estudos.felipe.gaspar@gmail.com>
2026-09-08 19:36:54 -0300

    Update Lista 1.c

--- Lista 1.c
+++ Lista 1.c
@@ -19,4 +19,33 @@ int main()
 
 */
 
+/*
+
 Lista 1 - Exercício: 2
+
+#include <stdio.h>
+
+int main()
+{
+    double valor, mantissa;
+    int expoente;
+    char buffer[50];
+    char mantissa_str[30]; 
+    
+    printf("Insira um valor: ");
+    scanf("%lf", &valor);
+    
+    sprintf(buffer, "%e", valor);
+    sscanf(buffer, "%[^e]e%d", mantissa_str, &expoente);
+    sscanf(mantissa_str, "%lf", &mantissa);
+    
+    printf("%.4lf X 10^%d\n", mantissa, expoente);
+    
+    return 0;
+}
+
+*/
+
+lista 1 - Exercício: 3
+
+
